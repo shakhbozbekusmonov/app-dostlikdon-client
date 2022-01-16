1e0195e176c45a67a19d6cd88b806f4cbff63f64
shakhbozbekusmonov <miracle_0319@bk.ru>
2022-01-16 20:55:01 +0500

    New Start

--- src/redux/actions/loginAction.js
+++ src/redux/actions/loginAction.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export function login(event, errors, values) {
     console.log(values);
 
-    axios.post("https://herokuapp.com/dostlikdon/api/auth/login", values)
+    axios.post("https://dostlikdon.herokuapp.com/auth/token/login", values)
         .then((res) => {
             console.log(res);
 
